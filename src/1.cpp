8ff3557af967711eff27f85b390d0970d81e2019
erustrum0 <157773321+erustrum0@users.noreply.github.com>
2024-02-06 22:56:39 +0000

    Finished, built program.

--- src/1.cpp
+++ src/1.cpp
@@ -1,15 +1,23 @@
 //Program 1: calculate area of triangle
-// area = (base*height)/2
+//Formula: area = (base*height)/2
+
+//embed libraries
 #include <iostream>
 using namespace std;
 
 int main() {
+    //get base and height
     double base;
     cout << "Base: ";
     cin >> base;
     double height;
     cout << "Height: ";
     cin >> height;
+
+    //calculate area
     double area = (base*height)/2;
+
+    //final output, terminate program
     cout << "Area: " << area << "\n";
+    return 0;
 }
