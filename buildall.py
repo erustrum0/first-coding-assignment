58c51623a615f433480fa07fda58ea38a458cbaa
erustrum0 <157773321+erustrum0@users.noreply.github.com>
2024-02-07 02:37:28 +0000

    Added minor comment

--- buildall.py
+++ buildall.py
@@ -4,14 +4,15 @@ import os
 #files is an array of files in the directory
 files = os.listdir(os.getcwd()+"/src")
 
-#let's remove non-cpp files.
+#let's remove non-cpp files in the array.
 for i in range(len(files)):
     print(files[i])
     if files[i].split(".")[1] != "cpp":
         files.pop(i)
-        print("Popped!")
+        print("Unwanted file popped!")
+        #no clue if this causes errors or not
 
-#files now contains only the cpp sources.
+#files array now contains only the cpp sources.
 #build these cpp programs, dump in builds folder.
 for i in range(len(files)):
     os.system("g++ src/" + files[i] + " -o builds/" + files[i].split(".")[0] + ".bin")
